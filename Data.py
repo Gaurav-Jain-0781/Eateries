c236559db42a0076a7da36240a11b1732df75842
Gaurav Jain <gauravjain0781@gmail.com>
2022-10-27 23:33:01 +0530

    Data file for the app

---
+++ Data.py
@@ -0,0 +1,100 @@
+names = {
+    1: "Gourmet",
+    2: "Mingos",
+    3: "Nandini",
+    4: "Fresheteria",
+    5: "Gourmet Extension",
+    6: "Ivy Hall",
+    7: "ICH",
+    8: "Just Bakes",
+    9: "Minus 1",
+    10: "Kiosk"
+}
+
+eatery = [{"Gourmet":
+                 {"Mingos":
+                      {"Sandwiches":
+                           {"Veg Sandwiches": 50,
+                            "Chicken Sandwich": 90,
+                            },
+                       "Shakes": {"Papaya": 50,
+                                  "Guava": 40}
+                      }
+                 }
+            },
+          {
+               "Nandini": {
+                   "Hot Beverages": {
+                       "Coffee / Tea / Badam Milk": 10,
+                       "Black Tea / Black Coffee": 10,
+                       "Ginger Coffee": 12,
+                       "Chocolate / Horlicks": 25,
+                       "Bournvita / Boast": 25,
+                       "Green Tea / Areca Tea": 15,
+                       "Plain Milk": 18,
+                       "Golden(Haldi) Milk": 20
+                   },
+                   "Ice Cream Scoops": {
+                       "Vanilla": 30,
+                       "Strawberry": 30,
+                       "Butter Scotch": 30,
+                       "Mango": 30,
+                       "Chocolate": 30,
+                       "Pista": 30,
+                       "Chocochip": 30,
+                       "Anjir": 30,
+                       "Kaju Draksh": 30,
+                       "Black Current": 30,
+                       "Tutti Frutti": 30,
+                   },
+                   "Cold Beverages": {
+                       "Oreo Milk Shake": 35,
+                       "Cold Coffee": 35,
+                       "Horlicks": 35,
+                       "Boost": 35,
+                       "Bournvita": 35,
+                       "Ice Cream Milk Shakes (All Flavours)": 35,
+                   },
+                   "Extras": {
+                       "Ice Cream Milk Shakes": 15,
+                       "Regular Cups": 1,
+                       "Jumbo Cups": 2,
+                       "Spoons": 1,
+                       "Cloth Bag": 5
+                   }
+               }
+           },
+          {
+               "Fresheteria": {
+                   "Fruit Juices": {
+                       "Papaya": 25,
+                       "Grape": 25,
+                       "Pineapple": 30,
+                       "Watermelon": 30,
+                       "Mosambi": 32
+                   },
+                   "Blenders": {
+                       "Imunity Booster": 25,
+                       "Melon Quencher": 30,
+                       "Summer Breeze": 35,
+                       "Purple Pine": 35,
+                       "Calcium Booster": 40,
+                       "ABC": 40
+                   },
+                   "Vegetables Juices": {
+                       "Tomatino": 20,
+                       "Cucumber Cooler": 20,
+                       "Beet It Up": 35,
+                       "Detox": 25,
+                       "Veggie Booster": 35
+                   },
+                   "Coolers": {
+                       "Fresh Lime": 15,
+                       "Masala Buttermilk": 15,
+                       "Mint Lemonade": 20,
+                       "Sweet Lassi": 30,
+                       "Fruit Lassi": 40
+                   }
+               }
+           }
+           ]
