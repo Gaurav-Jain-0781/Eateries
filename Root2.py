7a75a2662ce4956d17c32d03177a7a5c994afecd
Gaurav Jain <gauravjain0781@gmail.com>
2022-11-04 10:48:40 +0530

    Small changes

--- Root2.py
+++ Root2.py
@@ -1,9 +1,19 @@
 from Data import eatery
 import tkinter as tk
 
+button_height = 5
+button_width = 30
+
+frame_height = 5
+frame_width = 7
+
+
+def final():
+    print("abc")
+
 
 def gourmet():
-    data = eatery[1]
+    data = eatery[0]
     print(data)
     root_2 = tk.Tk()
     root_2.title(eatery)
@@ -19,7 +29,7 @@ def mingos():
     
 
 def nandini():
-    data = eatery[1]
+    data = eatery[2]
     print(data)
     root_2 = tk.Tk()
     root_2.title(eatery)
@@ -28,14 +38,35 @@ def nandini():
 
 def fresheteria():
     data = eatery[1]
-    print(data)
+    title = list(data.keys())
+
+    # creating new root for each eatery
     root_2 = tk.Tk()
-    root_2.title(eatery)
+
+    # basic configuration of root
+    root_2.geometry('400x600')
+    label = tk.Label(root_2, text="Fresheteria")
+    label.pack()
+    root_2.title(str(title))
+
+    # creating frames for app
+    frame = tk.Frame(root_2, height=frame_height, width=frame_width)
+
+    # buttons of root
+    values = data.values()
+    for v in values:
+        buttons = list(v.keys())
+
+    for b in buttons:
+        button = tk.Button(frame, text=b, height=button_height, width=button_width, command=lambda: final())
+        button.pack(side="left")
+
+    frame.pack()
     root_2.mainloop()
         
     
 def gourmet_extension():
-    data = eatery[1]
+    data = eatery[4]
     print(data)
     root_2 = tk.Tk()
     root_2.title(eatery)
@@ -43,7 +74,7 @@ def gourmet_extension():
 
 
 def ivy_hall():
-    data = eatery[1]
+    data = eatery[5]
     print(data)
     root_2 = tk.Tk()
     root_2.title(eatery)
@@ -51,7 +82,7 @@ def ivy_hall():
 
 
 def ich():
-    data = eatery[1]
+    data = eatery[6]
     print(data)
     root_2 = tk.Tk()
     root_2.title(eatery)
@@ -59,7 +90,7 @@ def ich():
 
 
 def just_bakes():
-    data = eatery[1]
+    data = eatery[7]
     print(data)
     root_2 = tk.Tk()
     root_2.title(eatery)
@@ -67,7 +98,7 @@ def just_bakes():
 
 
 def minus_1():
-    data = eatery[1]
+    data = eatery[8]
     print(data)
     root_2 = tk.Tk()
     root_2.title(eatery)
@@ -75,7 +106,7 @@ def minus_1():
 
 
 def kiosk():
-    data = eatery[1]
+    data = eatery[9]
     print(data)
     root_2 = tk.Tk()
     root_2.title(eatery)
